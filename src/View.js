f05d43d69d663481cd56df76b5636d56cf077786
NadezhdaLobacheva <nadyushland@gmail.com>
2025-08-22 15:42:49 +0300

    View-function

--- src/View.js
+++ src/View.js
@@ -1,16 +1,30 @@
+const chalk = require('chalk').default;
+const figlet = require('figlet');
 // Сделаем отдельный класс для отображения игры в консоли.
 
 class View {
   render(track, kills) {
-    const yourTeamName = 'Elbrus';
+    const yourTeamName = 'Werewolves';
 
     // Тут всё рисуем.
     console.clear();
+    figlet('Welcome to the poopies battle', (_, data) => {
+      console.log(chalk.yellowBright(data));
+    });
+    console.log('\n');
+    console.log(
+      chalk.greenBright('____________________________________________________________'),
+    );
+    console.log('\n');
     console.log(track.map((el) => el.join('')).join('\r\n'));
+    console.log('\n');
+    console.log(
+      chalk.greenBright('____________________________________________________________'),
+    );
+    console.log('\n');
+    console.log(chalk.yellowBright(`Убито врагов: ${kills} 💩`));
     console.log('\n\n');
-    console.log(`Убтито врагов: ${kills}`)
-    console.log('\n\n');
-    console.log(`Created by "${yourTeamName}" with love`);
+    console.log(chalk.greenBright(`Created by "${yourTeamName} 🐺" with love ❤`));
   }
 }
 
